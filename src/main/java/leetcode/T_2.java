80d06eb10054830db53b577f2efcab89bddf5fac
wang_cr7 <wang_cr7@126.com>
2022-08-28 16:45:38 +0800

    leet 2

--- src/main/java/leetcode/T_2.java
+++ src/main/java/leetcode/T_2.java
@@ -20,6 +20,30 @@ public class T_2 {
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
-        return null;
+        ListNode head = null, tail = null;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int num1 = l1 != null ? l1.val : 0;
+            int num2 = l2 != null ? l2.val : 0;
+            int sum = num1 + num2 + carry;
+            if (head == null) {
+                head = tail = new ListNode(sum%10);
+            } else {
+                tail.next = new ListNode(sum%10);
+                tail = tail.next;
+            }
+
+            carry = sum / 10;
+            if (l1 != null){
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        if (carry > 0) {
+            tail.next = new ListNode(carry);
+        }
+        return head;
     }
 }
