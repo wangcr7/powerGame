8cc7602efb661621a36d3405d61f712f3f1a80fa
wang_cr7 <wang_cr7@126.com>
2022-08-28 16:09:33 +0800

    leet 1

--- src/main/java/leetcode/T_1.java
+++ src/main/java/leetcode/T_1.java
@@ -1,5 +1,8 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @Title: T_1 两数之和
  * @Package: leetcode
@@ -13,14 +16,48 @@ package leetcode;
  */
 public class T_1 {
 
+    /**
+     * 暴力解法
+     * @param nums
+     * @param target
+     * @return
+     */
     public int[] twoSum(int[] nums, int target) {
 
-        return null;
+        int n = nums.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[0];
+    }
+
+    /**
+     * hash表遍历
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum1(int[] nums, int target) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+
+        }
+        return new int[0];
     }
 
     public static void main(String[] args) {
-
-        System.out.printf("aaa");
+//        System.out.printf("aaa");
 
     }
 }
